cd46c1df9b02cb58f9d8a3449e1c9b795da2920b
Praneeth-Gandodi <praneethgandhodi@gmail.com>
2026-05-11 00:19:01 +0530

    Disable autoformat for java and python

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -4,10 +4,8 @@ return {
     formatters_by_ft = {
       html = { "prettier" },
       css = { "prettier" },
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
+      java = {},
+      python = {},
     },
   },
 }
